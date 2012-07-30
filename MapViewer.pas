372753c6c18a4652a7190602215763f372cf86ce
Alexander Savinoff <hinstance@yandex.ru>
2012-07-31 01:38:35 +0400

    Modified. DND

--- MapViewer.pas
+++ MapViewer.pas
@@ -17,6 +17,7 @@ uses
   zgl_keyboard,
   zgl_sprite_2d,
   zgl_fx,
+  zgl_render_target,
 
   LogEntity,
   LogEntityFace,
@@ -40,12 +41,14 @@ type
     fFieldWidth, fFieldHeight: single;
     fXSpeed, fYSpeed: single;
     fMap: IMapData;
-    fMatrix: TCells.TMatrix; // direct access to Map.Cells.Matrix
+    fMatrix: TCells.TMatrix; // direct access to Map.Cells.Matrix; cached
+    fMapWidth, fMapHeight: integer; // direct access to Map.Cells.Width and Map.Cells.Height; cached
     fTerrain: ITerrainManager;
     fTerrainMan: TTerrainManager;
     fGridColor: LongWord;
     fGridAlpha: byte;
-    fFraming: TFPList;
+    fFramings: TFPList;
+    fFramingTexture: zglPRenderTarget;
     procedure Initialize;
     procedure AssignDefaults;
     procedure FocusViewOnMapCenter;
@@ -55,8 +58,10 @@ type
     procedure SetTerrain(const aTerrain: ITerrainManager);
     procedure ClearDrawn;
     procedure DrawTerrainLayerSubcolor(const aX, aY: integer; const aXD, aYD: single);
-    procedure ProcessFraming(const aX, aY: integer; const aXD, aYD: single);
+    function ProcessFraming(const aX, aY: integer; const aXD, aYD: single): boolean; inline;
     procedure DrawTerrainLayerSimple(const aX, aY: integer; const aXD, aYD: single);
+    procedure DrawFraming(const aP: pointer); inline;
+    procedure DrawFramings;
     procedure ReleaseFraming;
     procedure Finalize;
   public type
@@ -80,10 +85,14 @@ type
     property YSpeed: single read fYSpeed write fYSpeed;
     property Map: IMapData read fMap write fMap;
     property Matrix: TCells.TMatrix read fMatrix write fMatrix;
+    property MapWidth: integer read fMapWidth;
+    property MapHeight: integer read fMapHeight;
     property Terrain: ITerrainManager read fTerrain write SetTerrain;
     property TerrainMan: TTerrainManager read fTerrainMan;
     property GridColor: LongWord read fGridColor write fGridColor;
     property GridAlpha: byte read fGridAlpha write fGridAlpha;
+    property Framings: TFPList read fFramings;
+    property FramingTexture: zglPRenderTarget read fFramingTexture;
       // it is necessary to call this procedure after changing the map data
     procedure Update;
     procedure ReceiveInput(const aT: single);
@@ -102,12 +111,20 @@ uses
 
 type
   TDrawFraming = record
-    xd, yd: single;
+    xD, yD: single;
     Up, Down, Left, Right: boolean;
     typee: TTerrainType;
   end;
 
   PDrawFraming = ^TDrawFraming;
+
+procedure Clean(const aFraming: PDrawFraming); overload;
+begin
+  aFraming^.Up := false;
+  aFraming^.Down := false;
+  aFraming^.Left := false;
+  aFraming^.Right := false;
+end;
 
 function SortFraming(Item1, Item2: pointer): integer;
 var
@@ -115,8 +132,10 @@ var
 begin
   framing1 := PDrawFraming(Item1);
   framing2 := PDrawFraming(Item2);
-  if framing1^.typee > framing2^.typee then
-    result := 1
+  if framing1^.typee = framing2^.typee then
+    result := 0
+  else if framing1^.typee > framing2^.typee then
+    result := +1
   else
     result := -1;
 end;
@@ -189,7 +208,8 @@ begin
   pr2d_Rect(aXD, aYD, TileWidth+1, TileHeight+1, color, 255, PR2D_FILL);
 end;
 
-procedure TMapView.ProcessFraming(const aX, aY: integer; const aXD, aYD: single);
+function TMapView.ProcessFraming(const aX, aY: integer; const aXD, aYD: single
+  ): boolean;
 var
   c, U, D, L, R: TTerrainType;
   framing : PDrawFraming;
@@ -205,11 +225,15 @@ var
   function f: PDrawFraming; inline;
   begin
     if framing = nil then
+    begin
       New(framing);
+      Clean(framing);
+    end;
     result := framing;
   end;
 
 begin
+  //WriteLN(aX, ' ', aY);
   c := Matrix[aX, aY].typee;
   CleanUDLR;
   framing := nil;
@@ -221,7 +245,7 @@ begin
     f^.Up := true;
 
   // DOWN
-  if aY < Map.Cells.Height - 1 then
+  if aY < MapHeight - 1 then
     D := Matrix[aX, aY + 1].typee;
   if c <> D then
     f^.Down := true;
@@ -233,15 +257,18 @@ begin
     f^.Left := true;
 
   // RIGHT
-  if aX < Map.Cells.Width - 1 then
+  if aX < MapWidth - 1 then
     R := Matrix[aX + 1, aY].typee;
   if c <> R then
     f^.Right := true;
 
-  if framing = nil then exit;
-  framing^.xd := aXD;
-  framing^.yd := aYD;
-  fFraming.Add(framing);
+  result := Assigned(framing);
+  if not result then
+    exit;
+  framing^.xD := aXD;
+  framing^.yD := aYD;
+  framing^.typee := c;
+  Framings.Add(framing);
 end;
 
 procedure TMapView.DrawTerrainLayerSimple(const aX, aY: integer; const aXD, aYD: single);
@@ -249,43 +276,92 @@ var
   typee: TTerrainType;
   texture: zglPTexture;
   t: TTerrain;
+  isAnyFraming: boolean;
 begin
   typee := Matrix[aX, aY].typee;
   t := TerrainMan.Terrains[typee];
   texture := t.Texture;
   if texture = nil then
     exit;
-  ssprite2d_Draw(TerrainMan.SMask, aXD, aYD, TileWidth, TileHeight, 0);
-  ProcessFraming(aX, aY, aXD, aYD);
+  isAnyFraming := ProcessFraming(aX, aY, aXD, aYD);
+  {
+  if not isAnyFraming then
+    ssprite2d_Draw(texture, aXD, aYD, TileWidth, TileHeight, 0);
+  }
   {
   fx_SetBlendMode( FX_BLEND_MULT, FALSE );
-  ssprite2d_Draw(texture, aXD, aYD, TileWidth, TileHeight, 0);
+  ssprite2d_Draw(TerrainMan.SMask, aXD, aYD, TileWidth, TileHeight, 0);
   fx_SetBlendMode( FX_BLEND_NORMAL );
   }
 end;
 
+procedure TMapView.DrawFraming(const aP: pointer);
+var
+  f: PDrawFraming;
+  typee: TTerrainType;
+  t: TTerrain;
+  texture: zglPTexture;
+  xD, yD: single;
+begin
+  f := PDrawFraming(aP);
+  typee := f^.typee;
+  xD := f^.xD;
+  yD := f^.yD;
+  t := TerrainMan.Terrains[typee];
+  texture := t.Texture;
+  ssprite2d_Draw(texture, xD, yD, TileWidth, TileHeight, 0);
+  if f^.Up then
+  begin
+    rtarget_Set(FramingTexture);
+    fx_SetBlendMode(FX_BLEND_MASK);
+    ssprite2d_Draw(TerrainMan.SMask, 0, 0, TileWidth, TileHeight, 0);
+    fx_SetBlendMode(FX_BLEND_MULT, FALSE);
+    ssprite2d_Draw(texture, 0, 0, TileWidth, TileHeight, 0);
+    fx_SetBlendMode(FX_BLEND_NORMAL);
+    rtarget_Set(nil);
+    ssprite2d_Draw(FramingTexture^.Surface, xD, yD - TileHeight, TileWidth, TileHeight, 0);
+  end;
+end;
+
+procedure TMapView.DrawFramings;
+var
+  i: integer;
+begin
+  for i := 0 to Framings.Count - 1 do
+    DrawFraming(Framings[i]);
+end;
+
 procedure TMapView.ReleaseFraming;
 var
+  i: integer;
   p: pointer;
   f: PDrawFraming;
 begin
-  for p in fFraming do
+  for i := 0 to Framings.Count - 1 do
   begin
+    p := Framings[i];
     f := PDrawFraming(p);
     Dispose(f);
   end;
+  Framings.Clear;
 end;
 
 procedure TMapView.Finalize;
 begin
+  FreeAndNil(fFramings);
   FreeLog(fLog);
 end;
 
 procedure TMapView.Update;
 begin
   fMatrix := Map.Cells.Matrix;
+  fMapWidth := Map.Cells.Width;
+  fMapHeight := Map.Cells.Height;
   DetermineFieldDemensions;
   FocusViewOnMapCenter;
+
+  fFramings := TFPList.Create;
+  fFramingTexture := rtarget_Add(tex_CreateZero(TileWidth, TileHeight), RT_DEFAULT);;
 end;
 
 procedure TMapView.ReceiveInput(const aT: single);
@@ -322,10 +398,10 @@ end;
 
 procedure TMapView.DrawTerrainLayerSimples;
 begin
-  fFraming := TFPList.Create;
   ForeachVisibleCell(@DrawTerrainLayerSimple);
-  fFraming.Sort(@SortFraming);
-  FreeAndNil(fFraming);
+  Framings.Sort(@SortFraming);
+  DrawFramings;
+  ReleaseFraming;
 end;
 
   { Warning: some heavy calculations are taking place in this procedure }
@@ -365,15 +441,14 @@ begin
   yD := yA - yUA;
   yDD := yD; // store
   yy := y; // store
-  mcw := Map.Cells.Width;
-  mch := Map.Cells.Height;
+  mcw := MapWidth;
+  mch := MapHeight;
   while (x < mcw) and (xD < wndWidth) do
   begin
     y := yy;
     yD := yDD;
     while (y < mch) and (yD < wndHeight) do
     begin
-      //WriteLN('calling aP');
       aProcedure(x, y, xD, yD);
       y += 1;
       yD += TileHeight;
